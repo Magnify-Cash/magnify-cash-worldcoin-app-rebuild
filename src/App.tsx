5a17f8ac2523e011cf645552ad4ddfa435987c31
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2025-02-05 11:46:37 -0500

    fix: add minikit provider

--- src/App.tsx
+++ src/App.tsx
@@ -11,73 +11,76 @@ import Announcements from "@/pages/Announcements";
 import ProtectedRoute from "@/pages/ProtectedPage";
 import "./App.css";
 import eruda from "eruda";
+import { MiniKitProvider } from "./providers/MiniKitProvider";
 
 eruda.init();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/welcome" replace />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route
-          path="/announcements"
-          element={
-            <ProtectedRoute>
-              <Announcements />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/guide"
-          element={
-            <ProtectedRoute>
-              <Guide />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/wallet"
-          element={
-            <ProtectedRoute>
-              <Wallet />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/loan"
-          element={
-            <ProtectedRoute>
-              <Loan />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/repay-loan"
-          element={
-            <ProtectedRoute>
-              <RepayLoan />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/upgrade-verification"
-          element={
-            <ProtectedRoute>
-              <UpgradeVerification />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <MiniKitProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/welcome" replace />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route
+            path="/announcements"
+            element={
+              <ProtectedRoute>
+                <Announcements />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/guide"
+            element={
+              <ProtectedRoute>
+                <Guide />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/wallet"
+            element={
+              <ProtectedRoute>
+                <Wallet />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/loan"
+            element={
+              <ProtectedRoute>
+                <Loan />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/repay-loan"
+            element={
+              <ProtectedRoute>
+                <RepayLoan />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/upgrade-verification"
+            element={
+              <ProtectedRoute>
+                <UpgradeVerification />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </MiniKitProvider>
   );
 }
 
